67249f0be70121251af1f504ba1cfaa2cd77355b
Valensio Deva <devalensio@gmail.com>
2021-05-14 15:43:35 +0700

    add db uri for mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.DB_URI)],
+  imports: [MongooseModule.forRoot(process.env.DB_URI), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })

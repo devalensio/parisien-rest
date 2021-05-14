682abfcc637f074712d93bb9c76cba1cde340c7a
Valensio Deva <devalensio@gmail.com>
2021-05-14 15:20:34 +0700

    add helmet dependency

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+
+import * as helmet from 'helmet';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: console,
+  });
+
+  app.use(helmet());
+  app.enableCors();
+  app.setGlobalPrefix('v1');
+
   await app.listen(3000);
 }
 bootstrap();

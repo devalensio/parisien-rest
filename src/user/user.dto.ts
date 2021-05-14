512165fa17cb004c332c96927e4faa305663d701
Valensio Deva <devalensio@gmail.com>
2021-05-14 16:29:46 +0700

    add user dto as type

---
+++ src/user/user.dto.ts
@@ -0,0 +1,12 @@
+export class UserDto {
+  readonly name: String;
+  readonly email: String;
+  readonly passwordHash: String;
+  readonly phone: String;
+  readonly isAdmin: Boolean;
+  readonly street: String;
+  readonly apartment: String;
+  readonly zip: String;
+  readonly city: String;
+  readonly country: String;
+}
